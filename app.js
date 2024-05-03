15e86c253ce9444f5befee72a0ae2014abce8504
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-05-04 01:11:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+//q1
+// let emptyArray = [[]]
+
+
+//q3
+// for (let i = 1; i <= 10; i++)
+// console.log(i)
+
+
+// //q4
+// let num = +prompt("Enter the number of multiplication table")
+// let length = +prompt("Enter the length of the multiplication table")
+
+// console.log(`Multiplication table for ${num} with length ${length} is:`)
+// for (let i = 1; i <= length; i++)
+// console.log(`${num} x ${i} = ${num*i}`);
+
+
+
+
+
+//q5
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// console.log("Printing items using for loop:");
+
+// for(let i = 0; i < fruits.length; i++)
+// console.log(fruits[i])
+
+
+
+
+//q6
+// console.log("Counting:");
+// for (let i = 0; i <= 15; i++)
+// console.log(i)
+
+// console.log("Reverse Counting")
+// for (let i = 10; i >=1; i--)
+// console.log(i)
+
+
+// console.log("Even:")
+// for (let i = 0 ; i <= 20 ; i = i+2)
+// console.log(i)
+
+
+// console.log("Odd:")
+// for (let i = 1; i <= 19 ; i = i+2)
+// console.log(i)
+
+
+// console.log("Series")
+// for (let i = 2; i <= 20; i = i + 2)
+// console.log(`${i}k`)
+
+
+
+
+
+
+
+//
